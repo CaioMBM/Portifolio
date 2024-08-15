aedcb19d04499736a2179fc028c9363185d18bb9
CaioMBM <caiombmonteiro@gmail.com>
2024-08-15 02:13:35 -0300

    Criei as functions para acessar o repositório ou o projeto em si do projeto "Tela de Login Simples"

---
+++ js/redirecionar.js
@@ -0,0 +1,10 @@
+// PROJETO - TELA DE LOGIN
+// Leva para o repositório
+function abrirRepositorioTelaLogin(){
+    window.open("https://github.com/CaioMBM/tela-de-login-sport");
+}
+
+// Leva para o projeto
+function abrirProjetoTelaLogin(){
+    window.open("https://caiombm.github.io/tela-de-login-sport/");
+}
