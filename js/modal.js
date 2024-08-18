7d31216dae744bc3914cb96e960ad3d68e2ed052
CaioMBM <caiombmonteiro@gmail.com>
2024-08-18 20:56:19 -0300

    Fazendo modal "Cálculo IMC" aparecer

--- js/modal.js
+++ js/modal.js
@@ -13,10 +13,12 @@ var tituloRecriando = document.getElementById("tituloRecriando");
 // Seleciona todos os elementos que possuem a classe 'modal-display'
 var modals         = document.querySelectorAll(".modal-display");
 var modalTelaLogin = modals[0];
+var modalIMC = modals[1];
 
 // Seleciona todos os elementos que possuem a classe 'icon-modal-fechar'
 var fecharModal          = document.querySelectorAll(".icon-modal-fechar");
 var fecharModalTelaLogin = fecharModal[0]; 
+var fecharModalIMC = fecharModal[1]; 
 
 
 
@@ -24,36 +26,53 @@ var fecharModalTelaLogin = fecharModal[0];
 projetoTelaLogin.addEventListener("click", function(){
         // Projeto respectivo
         projetoTelaLogin.style.display = "none";
-        tituloTelaLogin.style.display = "none";
-        modalTelaLogin.style.display = "block";
+        tituloTelaLogin.style.display  = "none";
+        modalTelaLogin.style.display   = "block";
 
         // Outros projetos
         projetoIMC.style.display = "none";
-        tituloIMC.style.display = "none";
+        tituloIMC.style.display  = "none";
 
         projetoConversor.style.display = "none";
-        tituloConversor.style.display = "none";
+        tituloConversor.style.display  = "none";
 
         projetoRecriando.style.display = "none";
-        tituloRecriando.style.display = "none";
+        tituloRecriando.style.display  = "none";
 });
 
 // TELA DE LOGIN - Esconder modal
 fecharModalTelaLogin.addEventListener("click", function(){
-        console.log("FECHOU");
-
         // Projeto respectivo
         projetoTelaLogin.style.display = "block";
-        tituloTelaLogin.style.display = "block";
-        modalTelaLogin.style.display = "none";
+        tituloTelaLogin.style.display  = "block";
+        modalTelaLogin.style.display   = "none";
 
         // Outros projetos
         projetoIMC.style.display = "block";
-        tituloIMC.style.display = "block";
+        tituloIMC.style.display  = "block";
 
         projetoConversor.style.display = "block";
-        tituloConversor.style.display = "block";
+        tituloConversor.style.display  = "block";
 
         projetoRecriando.style.display = "block";
-        tituloRecriando.style.display = "block";
+        tituloRecriando.style.display  = "block";
 });
+
+
+// CÁLCULO IMC - Exibir modal
+projetoIMC.addEventListener("click", function(){
+        // Projeto respectivo
+        projetoIMC.style.display = "none";
+        tituloIMC.style.display  = "none";
+        modalIMC.style.display   = "block";
+
+        // Outros projetos
+        projetoTelaLogin.style.display = "none";
+        tituloTelaLogin.style.display  = "none";
+
+        projetoConversor.style.display = "none";
+        tituloConversor.style.display  = "none";
+
+        projetoRecriando.style.display = "none";
+        tituloRecriando.style.display  = "none";
+})
